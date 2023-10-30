b22e0190f6c636b84dab30593457f234ceec52fe
ttittitti <ttittitti@github.com>
2023-10-30 14:23:15 +0900

    'sync'

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TTITTITTI",
+    pageTitle: "TTT",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
