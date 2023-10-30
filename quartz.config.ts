f3ce524a5833730abeeb713908ee6f86ff963202
ttittitti <ttittitti@github.com>
2023-10-30 14:15:17 +0900

    'sync'

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "HyunSu Jeong",
+    pageTitle: "TTITTITTI",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "cookie3029.vercel.app",
+    baseUrl: "ttittitti.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
